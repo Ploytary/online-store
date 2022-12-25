a50bcb1d219834bdc502e29db20784cc64090cba
Dmitrii Grigorev <ploytary.fed@gmail.com>
2022-12-25 15:43:48 +0300

    refactor: cleaning experimental code, preparing gh deploy

--- src/app/main/render.ts
+++ src/app/main/render.ts
@@ -1,11 +1,7 @@
-import { BrandList } from './components/brand-list/brand-list';
-import { SubmitButton } from './components/button/submit-button';
 import { CheckBox } from './components/checkbox/checkbox';
 
 export const renderListPloytary = [
-  new SubmitButton({content: 'как в реакте', classList: ['base-button'], dataSpecialDataId: 'megaID'}),
   new CheckBox({name: 'check1', value: 'checkvalue', content: 'шоколадница', dataItemId: 'custom-data'}),
-  new BrandList({})
 ]
 export const renderListmrdmitrij = [
   new CheckBox({name: 'просрал', value: 'всё', content: 'на свете', dataItemId: 'downgrade'}),
