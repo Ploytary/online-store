397b5304608a7f767809b2cc09af41290edaa3ba
Dmitrii Grigorev <ploytary.fed@gmail.com>
2022-12-27 23:53:12 +0300

    feat: add price-range-slider component

--- src/app/main/render.ts
+++ src/app/main/render.ts
@@ -1,13 +1,16 @@
 import { CheckBox } from './components/checkbox/checkbox';
 import { BrandList } from './components/brand-list/brand-list';
 import { CategoryList } from './components/category-list/category-list';
+import { RangeSlider } from './components/range-slider/range-slider';
+import { PriceRangeSlider } from './components/price-range-slider/price-range-slider';
 
 export const renderListPloytary = [
   new BrandList(),
   new CategoryList(),
+  new PriceRangeSlider(),
 ]
 export const renderListmrdmitrij = [
-  new CheckBox({name: 'просрал', value: 'всё', content: 'на свете', dataItemId: 'downgrade'}),
+  new CheckBox({name: 'просрал', value: 'всё', content: 'на свете', dataItemId: 'downgrade', classList: ['ппц']}),
 ]
 
 export function render(elementList: object): HTMLElement {
